a190f1314c225b62ae90c5dd2d37eec0f622a900
a1824jj <a1824jj@aiit.ac.jp>
2020-04-17 16:52:47 +0900

    implement user logout

--- like_instagram_used_by_Parse/FeedVC.swift
+++ like_instagram_used_by_Parse/FeedVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class FeedVC: UIViewController {
 
@@ -15,16 +16,30 @@ class FeedVC: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logoutClicked(_ sender: Any) {
+        PFUser.logOutInBackground { (error) in
+            if error != nil {
+                self.alert(title: "Error", message: error?.localizedDescription ?? "Unknow error")
+            } else {
+                print("logout")
+                
+                UserDefaults.standard.removeObject(forKey: "username")
+                UserDefaults.standard.synchronize()
+                
+                let signIn = self.storyboard?.instantiateViewController(withIdentifier: "signUp") as! SigninVC
+                
+                let sceneDelegate = UIApplication.shared.connectedScenes.first!.delegate as! SceneDelegate
+                sceneDelegate.window!.rootViewController = signIn
+                
+            }
+        }
     }
-    */
-
+    
+    func alert(title: String, message: String) {
+        let alert =  UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
