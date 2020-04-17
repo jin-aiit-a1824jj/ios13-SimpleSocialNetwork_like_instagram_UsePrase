9cf368f1487ff2ee04457559a8699ebea71cdb08
a1824jj <a1824jj@aiit.ac.jp>
2020-04-17 16:16:03 +0900

    implement sign up user

--- like_instagram_used_by_Parse/SigninVC.swift
+++ like_instagram_used_by_Parse/SigninVC.swift
@@ -14,31 +14,9 @@ class SigninVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        /*
-        let parseObject = PFObject(className: "Fruits")
-        parseObject["name"] = "apple"
-        parseObject["calories"] = 100
-        parseObject.saveInBackground { (success, error) in
-            if error != nil {
-                print(error?.localizedDescription as Any)
-            } else {
-                print("Successful!")
-            }
-        }
-        */
+
         
-        let query = PFQuery(className: "Fruits")
-        query.whereKey("calories", greaterThan: 50)
-        query.findObjectsInBackground { (objects, error) in
-            if error != nil {
-                print(error?.localizedDescription as Any)
-            } else {
-                print(objects as Any)
-            }
-        }
-        
-        
+
     }
     
 
@@ -51,7 +29,29 @@ class SigninVC: UIViewController {
     }
     
     @IBAction func signUpClicked(_ sender: Any) {
+        if userNameText.text != "" && passwordText.text != "" {
+            
+            let user = PFUser()
+            user.username = self.userNameText.text!
+            user.password = self.passwordText.text!
+            user.signUpInBackground { (success, error) in
+                if error != nil {
+                    self.alert(title: "Error", message: error?.localizedDescription ?? "Unknown error" )
+                }else{
+                    print("user create")
+                }
+            }
+            
+        } else {
+            self.alert(title: "Error", message: "Username needed!")
+        }
+    }
     
+    func alert(title: String, message: String) {
+        let alert =  UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
